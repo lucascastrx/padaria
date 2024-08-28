777d73163e90fd8495978f0bcdc49f02dbf06a89
Lucas <luukascastro@gmail.com>
2024-08-28 09:35:37 -0300

    feat: Initial structures for Tickets

---
+++ Padaria/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padaria
+{
+    internal class Ticket
+    {
+        public int Id { get; set; }
+        public List<ProductTicket> ProductTickets { get; set; }
+        public Client Client { get; set; }
+        public PaymentType PaymentType { get; set; }
+
+        public double TotalValue()
+        {
+            var amount = 0;
+            foreach(var ticket in ProductTickets)
+            {
+                amount += ticket.Value;
+            }
+
+            return amount;
+        }
+        
+
+    }
+}
---
+++ Padaria/TicketRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padaria
+{
+    internal class TicketRepository : IRepository<int, Ticket>
+    {
+        private List<Ticket> tickets = [];
+        private static int TICKET_ID;
+        public Ticket Create(Ticket Data)
+        {
+            Data.Id = TICKET_ID++;
+            tickets.Add(Data);
+            return Data;
+
+        }
+
+        public void Delete(int Id)
+        {
+            var ticket = tickets.Find(o => o.Id == Id);
+            if (ticket == null)
+                throw new Exception("Ticket not found.");
+            tickets.Remove(ticket);
+        }
+
+        public Ticket Read(int Id)
+        {
+            var ticket = tickets.Find(o => o.Id == Id);
+            if (ticket == null)
+                throw new Exception("Ticket not found.");
+            return ticket;
+        }
+
+        public Ticket Update(int Id, Ticket data)
+        {
+            var ticket = tickets.Find(o => o.Id == Id);
+            if (ticket == null)
+                throw new Exception("Ticket not found.");
+            ticket. = data.;
+            ticket. = data.;
+            return ticket;
+
+        }
+    }
+}
